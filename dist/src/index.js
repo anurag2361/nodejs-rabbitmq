a4378259ee1c20af91f929fff0f502ef0102ce28
anurag2361 <sharmaanurag.236@gmail.com>
2019-10-17 12:41:11 +0530

    added dotenv

--- dist/src/index.js
+++ dist/src/index.js
@@ -2,15 +2,27 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const callback_api_1 = __importDefault(require("amqplib/callback_api"));
+const dotenv = __importStar(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
 const app = express_1.default();
 const route_1 = __importDefault(require("./routes/route"));
-const port = 9000;
+if (fs_1.default.existsSync(".env")) {
+    console.log("Using .env file to supply config environment variables");
+    dotenv.config({ path: ".env" });
+}
 app.use(express_1.default.json({ limit: "1mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
-callback_api_1.default.connect("amqp://localhost", (err, conn) => {
+callback_api_1.default.connect(process.env.RABBIT_URL, (err, conn) => {
     if (err) {
         throw err;
     }
@@ -20,12 +32,12 @@ callback_api_1.default.connect("amqp://localhost", (err, conn) => {
     }
 });
 app.use("/user", route_1.default);
-app.listen(port, (err) => {
+app.listen(process.env.APP_PORT, (err) => {
     if (err) {
         throw err;
     }
     else {
-        console.log(`App running on http://localhost:${port}`);
+        console.log(`App running on http://localhost:${process.env.APP_PORT}`);
     }
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import amqp from "amqplib/callback_api";    // importing rabbitMQ library
+import * as dotenv from "dotenv";
 import express, { Express } from "express";
+import fs from "fs";
 const app: Express = express();
 import router from "./routes/route";
-const port: number = 9000;
+
+if (fs.existsSync(".env")) {
+    console.log("Using .env file to supply config environment variables");
+    dotenv.config({ path: ".env" });
+}
 
 // using express body-parser for form data
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // creating a single connection to Rabbitmq and exporting connection object;
-amqp.connect("amqp://localhost", (err, conn: amqp.Connection) => {
+amqp.connect(process.env.RABBIT_URL, (err, conn: amqp.Connection) => {
     if (err) {
         throw err;
     } else {
@@ -20,10 +26,10 @@ amqp.connect("amqp://localhost", (err, conn: amqp.Connection) => {
 
 app.use("/user", router);
 
-app.listen(port, (err) => {
+app.listen(process.env.APP_PORT, (err) => {
     if (err) {
         throw err;
     } else {
-        console.log(`App running on http://localhost:${port}`);
+        console.log(`App running on http://localhost:${process.env.APP_PORT}`);
     }
 });
